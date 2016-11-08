21f645f7def69aa46fd63b2768dde3a6eb1c0cc6
alexy <alexyvercruysse@gmail.com>
2016-11-08 15:03:13 +0100

    Connect db with app

--- app/src/main/java/com/example/iem/tubbourg_en_bresse/menu.java
+++ app/src/main/java/com/example/iem/tubbourg_en_bresse/menu.java
@@ -1,15 +1,26 @@
 package com.example.iem.tubbourg_en_bresse;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class menu extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
 
     }
     public void goToMenuTrajet(View v){
@@ -19,5 +30,32 @@ public class menu extends AppCompatActivity {
     public void goToLigne(View v){
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu_main; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuSearchInterval:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.more:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
